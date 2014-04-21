8b679b1ade523b7fdb2d71e8cce296581a216ccd
Aidi othmane <aidi.othmane@gmail.com>
2014-04-21 02:24:49 +0200

    Ajout de Test du classe domaine Livre

--- Biblioj-BA/test/unit/biblioj/LivreTests.groovy
+++ Biblioj-BA/test/unit/biblioj/LivreTests.groovy
@@ -11,7 +11,38 @@ import org.junit.*
 @TestFor(Livre)
 class LivreTests {
 
-    void testSomething() {
-       fail "Implement me"
-    }
+	void testConstraintTitreNullNombreExemplairesNullNombreExemplairesDisponiblesNull() {
+		def livre0 = new Livre()
+		assertFalse livre0.validate()
+	 }
+	 
+	 void testConstraintTitreNull() {
+		 def livre = new Livre(nombreExemplaires : 5, nombreExemplairesDisponibles : 5, type : new TypeDocument(intitule : "Nouveaut�"))
+		 assertFalse livre.validate()
+	 }
+	 
+	 void testConstraintNombreExemplairesMinValue() {
+		 def livre = new Livre(titre : "LivreAdo", nombreExemplaires : -1, nombreExemplairesDisponibles : 7, type : new TypeDocument(intitule : "Nouveaut�"))
+		 assertFalse livre.validate()
+	 }
+	 
+	 void testConstraintNombreExemplairesDisponiblesMinValue() {
+		 def livre = new Livre(titre : "LivreAdo", nombreExemplaires : 5, nombreExemplairesDisponibles : -1, type : new TypeDocument(intitule : "Nouveaut�"))
+		 assertFalse livre.validate()
+	 }
+	 
+	 void testConstraintTypeDocumentNull() {
+		 def livre = new Livre(titre : "LivreAdo", nombreExemplaires : 6, nombreExemplairesDisponibles : 7)
+		 assertTrue livre.validate()
+	 }
+	 
+	 void testConstraintsValidees() {
+		 def livre = new Livre(titre : "LivreAdo", nombreExemplaires : 7, nombreExemplairesDisponibles : 10, type : new TypeDocument(intitule : "Nouveaut�"))
+		 assertTrue livre.validate()
+	 }
+	 
+	 void testToString() {
+		 def livre = new Livre(titre : "LivreAdo", nombreExemplaires : 20, nombreExemplairesDisponibles : 5, type : new TypeDocument(intitule : "Nouveaut�"))
+		 assertEquals("LivreAdo", livre.toString())
+	 }
 }
