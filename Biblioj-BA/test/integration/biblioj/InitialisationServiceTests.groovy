d1c313401880f23ae16cd1de2a15de001e06f03f
anasbenis <anasbenis@hotmail.com>
2014-04-23 00:50:51 +0200

    70% coverage

--- Biblioj-BA/test/integration/biblioj/InitialisationServiceTests.groovy
+++ Biblioj-BA/test/integration/biblioj/InitialisationServiceTests.groovy
@@ -2,20 +2,16 @@ package biblioj
 
 import static org.junit.Assert.*
 
-import grails.test.mixin.*
-import grails.test.mixin.support.*
 import org.junit.*
 
-/**
- * See the API for {@link grails.test.mixin.support.GrailsUnitTestMixin} for usage instructions
- */
-@TestMixin(GrailsUnitTestMixin)
 class InitialisationServiceTests {
 
+    @Before
     void setUp() {
         // Setup logic here
     }
 
+    @After
     void tearDown() {
         // Tear down logic here
     }
@@ -27,5 +23,5 @@ class InitialisationServiceTests {
 		assertEquals(11, Livre.count())
 		assertEquals(0, Reservation.count())
 		assertEquals(3, TypeDocument.count())
-	}
-}
+
+	}}
